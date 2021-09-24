de983996126986c5b1e0b2a455c9e58e83e82da6
Alexander Bryand Koroh <75639365+alexkoroh@users.noreply.github.com>
2021-09-25 01:35:43 +0800

    Operrator dalam Javascript

---
+++ Operator Assignment.js
@@ -0,0 +1,24 @@
+// ! Operator dalam Jvascript ...(Operator Assignment)
+
+let a = 1
+
+a+= 2 // Nilai a di tambah 2
+
+a-= 2  // Nilai a di kurang 2
+
+
+a*= 2 // Nilai a di kali 2
+
+
+a/= 2 // Nilai a di bagi 2
+
+
+a%= 2 // Nilai a di modulus 2
+
+
+a**= 2 // Nilai a di modulus 2
+
+
+
+console.log(a);
+
---
+++ Operator Javascript.js
@@ -0,0 +1,13 @@
+// ! Operator dalam Jvascript ...(Operator Aritmatika)
+
+let a = 3 + 2 // disini operator aritmatika adalah tanda +
+let b = 3 - 2 // disini operator aritmatika adalah tanda -
+let c = 3 * 2 // disini operator aritmatika adalah tanda *
+let d = 3 / 2 // disini operator aritmatika adalah tanda /
+let e = 3 % 2 // disini operator aritmatika adalah tanda %
+let f = 3 ** 2 // disini operator aritmatika adalah tanda **
+
+console.log(a,b,c,d,e,f);
+
+//! Tanda % itu adalah sisa bagi
+//! Tanda ** itu pangkat 2
